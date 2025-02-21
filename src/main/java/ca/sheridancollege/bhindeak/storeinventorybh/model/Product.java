c637cfc48e52687cbc39c7234fe5965aa864c55d
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-02-21 11:55:27 -0500

    Store and product java class has been created

---
+++ src/main/java/ca/sheridancollege/bhindeak/storeinventorybh/model/Product.java
@@ -0,0 +1,18 @@
+package ca.sheridancollege.bhindeak.storeinventorybh.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
