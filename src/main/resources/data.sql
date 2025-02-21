e4e5d9465bc26aa2190e32b11bcaaeb2133118ea
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-02-21 11:50:26 -0500

    Sql file has been created

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO store (name) VALUES ('Walmart');
+INSERT INTO store (name) VALUES ('Costco');
+INSERT INTO store (name) VALUES ('No Frills');
+
+INSERT INTO product (name) VALUES ('Orange');
+INSERT INTO product (name) VALUES ('Banana');
+INSERT INTO product (name) VALUES ('Apple');
