878c96384a23ca308e9af365b0aa64bde8b24408
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-02-21 11:47:54 -0500

    intial commit

---
+++ src/main/java/ca/sheridancollege/bhindeak/storeinventorybh/StoreInventorybhApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.storeinventorybh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInventorybhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreInventorybhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/bhindeak/storeinventorybh/StoreInventorybhApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bhindeak.storeinventorybh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreInventorybhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
